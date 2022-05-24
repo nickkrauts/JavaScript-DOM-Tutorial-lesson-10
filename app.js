521bb126030fb98154ecbf1e3faae6afbfe65169
Nak14 <nickkraut@gmail.com>
2022-05-24 13:08:36 -0700

    complete Net-Ninja DOM 1-5

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const list = document.querySelector('#book-list ul');
+/* TUT 4: select the child in an element
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wmf);
+*/
+//select all elements
 
-// delete books
+let books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach(function(book){
+  book.textContent += ' new text'
+
+})
+
+
+
+/* delete books
+const list = document.querySelector('#book-list ul');
 list.addEventListener('click', (e) => {
   if(e.target.className == 'delete'){
     const li = e.target.parentElement;
     li.parentNode.removeChild(li);
   }
-});
+}); */
+
+
+
+/*tutorial 3 example: turn HTML collection into an array than get each element in said array. 
+let titles = document.getElementsByClassName('title');
+Array.from(titles).forEach(function(item){
+  console.log(item); 
+}) */
